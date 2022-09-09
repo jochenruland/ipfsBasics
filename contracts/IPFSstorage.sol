288743952ff1fcb2117be86d80211ef5da127725
jochenruland <jochenhgruland@gmail.com>
2022-09-09 08:40:05 +0200

    BUGFIX parameter in external function setFile() changed to calldata

--- contracts/IPFSstorage.sol
+++ contracts/IPFSstorage.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 contract IPFSstorage {
   mapping(address => string) public metadataURLs;
 
-  function setFile(string memory metadataURL) external {
+  function setFile(string calldata metadataURL) external {
     metadataURLs[msg.sender] = metadataURL;
   }
 }
