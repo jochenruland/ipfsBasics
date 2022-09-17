9bb3cfc5ca2ce277bae2790c2b03d28d7defde04
jochenruland <jochenhgruland@gmail.com>
2022-09-17 10:27:40 +0200

    EDIT function parameters with _ in setFile()

--- contracts/IPFSstorage.sol
+++ contracts/IPFSstorage.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 contract IPFSstorage {
   mapping(address => string) public metadataURLs;
 
-  function setFile(string calldata metadataURL) external {
-    metadataURLs[msg.sender] = metadataURL;
+  function setFile(string calldata _metadataURL) external {
+    metadataURLs[msg.sender] = _metadataURL;
   }
 }
